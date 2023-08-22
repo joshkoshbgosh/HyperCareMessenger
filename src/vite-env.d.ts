31f103c3727fe28bd22846b0ce4286be15061b2b
jasokant-klick <josh@tryhellohire.com>
2023-08-22 14:59:52 -0400

    add api auth environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GQL_TOKEN: string;
+  readonly VITE_GQL_SCOPE: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
